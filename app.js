3413ceff724ac89d2d479998ca2c37f7fcb809e0
Kelly Kim <kellykim408@gmail.com>
2020-10-13 01:44:53 -0700

    add intern question sections

--- app.js
+++ app.js
@@ -43,94 +43,144 @@ function managerPrompt() {
         },
     ])
 
-            .then(function(response) {
-                let managerName = response.managerName;
-                let managerId = response.managerId;
-                let managerEmail = response.managerEmail;
-                let officeNumber = response.officeNumber;
-                let manager = new Manager(
-                    managerName,
-                    managerId,
-                    managerEmail,
-                    officeNumber
-                );
+        .then(function (response) {
+            let managerName = response.managerName;
+            let managerId = response.managerId;
+            let managerEmail = response.managerEmail;
+            let officeNumber = response.officeNumber;
+            let manager = new Manager(
+                managerName,
+                managerId,
+                managerEmail,
+                officeNumber
+            );
 
-                team.push(manager);
-                employeeId++;
-                console.log('Now its time to build your team!');
+            team.push(manager);
+            employeeId++;
+            console.log('Now its time to build your team!');
 
-                employeePrompts();
+            employeePrompts();
 
+        });
+}
+
+function employeePrompts() {
+    return inquirer.prompt([
+
+        {
+            type: "list",
+            name: "employeeTitle",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern"],
+        },
+        {
+            type: "input",
+            name: "employeeName",
+            message: "What is the employee's name?",
+        },
+        {
+            type: "input",
+            name: "employeeEmail",
+            message: "What is the employee's email address?",
+        },
+    ])
+
+        .then(function (response) {
+            let employeeTitle = response.employeeTitle;
+            let employeeName = response.employeeName;
+            let employeeEmail = response.employeeEmail;
+
+            if (employeeTitle === "Engineer") {
+                inquirer.prompt([
+
+                    {
+                        type: "input",
+                        name: "gitHubUsername",
+                        message: "What is your Github Username?"
+                    },
+                    {
+                        type: "list",
+                        name: "otherEmployees",
+                        message: "Which type of team member would you like to add?",
+                        choices: ["Yes", "No"],
+                    }
+
+                ])
+                    .then(function (response) {
+                        let gitHubUsername = response.gitHubUsername;
+                        let engineer = new Engineer(
+                            employeeName,
+                            employeeId,
+                            employeeEmail,
+                            gitHubUsername
+                        );
+
+                        team.push(engineer);
+                        employeeId++;
+
+                        if (response.otherEmployees === "Yes") {
+                            employeePrompts();
+                        }else{
+                            generateTeamPage();
+                            return;
+                            }
+                        });
+                    } else{
+
+
+                        return inquirer.prompt ([
+                            {
+                                type: "input",
+                                name: "schoolName",
+                                message: "What is the name of the school that your intern attends?"
+                            }, 
+                            {
+                                type: "list",
+                                name: "otherEmployees",
+                                message: "Which type of team member would you like to add?",
+                                choices: ["Yes", "No"],
+                            }
+                        ])
+                        .then(function(response) {
+                            let internSchool = response.internSchool;
+                            let intern = new Intern(
+                                employeeName,
+                                employeeId,
+                                employeeEmail,
+                                internSchool
+                            );
+                            team.push(intern);
+                            employeeId++;
+                            
+                            if (response.otherEmployees === "Yes"){
+                                employeePrompts();
+                            } else {
+                                generateTeamPage();
+                                return;
+                            }
+                        });
+                    }
             });
         }
 
-        function employeePrompts() {
-           return inquirer.prompt([
-
-            {
-                type: "list",
-                name: "employeeTitle",
-                message: "Which type of team member would you like to add?",
-                choices: ["Engineer", "Intern"],
-            },
-            {
-                type: "input",
-                name: "teamMember",
-                message: "What is the employee's name?",
-            },
-            {
-                type: "input",
-                name: "employeeEmail",
-                message: "What is the employee's email address?",
-            },
-
-
-
-           ])
-        }
-
-       
-        {
-            type: "input",
-            name: "name",
-            message: "What is your engineer's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your engineer's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your engineer's email address?"
-        },
-        {
-            type: "input",
-            name: "gitHubUsername",
-            message: "What is your Github Username?"
-        },
+        function generateTeamPage(){
+            let allCards = "";
+            team.forEach(item => {
+                let cardString = item.createCard();
+                allCards += cardString;
+            });
         
-        {
-            type: "input",
-            name: "name",
-            message: "What is your intern's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your intern's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your interns's email address?"
-        },
-        {
-            type: "input",
-            name: "schoolName",
-            message: "What is the name of the school that your intern attends?"
-        },
+
+
+
+
+
+
+
+
+
+        
+            
     ]);
 }
 
@@ -154,7 +204,7 @@ promptUser()
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-function render(){
+function render() {
 
 };
 
@@ -165,7 +215,7 @@ function render(){
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-var outputPath; 
+var outputPath;
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
